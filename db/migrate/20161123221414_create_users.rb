8f1a6fb6111d50bb9fcb5d652aa3e7d864d62d84
pawelztef <pawelztef@gmial.com>
2016-11-23 22:46:10 +0000

    user model init, mysql connection

---
+++ db/migrate/20161123221414_create_users.rb
@@ -0,0 +1,13 @@
+class CreateUsers < ActiveRecord::Migration
+  def up 
+    create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :second_name, null: false
+      t.string :email, null: false
+      t.string :password_digest
+      t.timestamps null: false
+    end
+  end
+  def down
+    drop_table :users end
+end
