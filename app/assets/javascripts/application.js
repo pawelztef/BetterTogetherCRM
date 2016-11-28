2e2e0a453c0246334c97eb0014c9a629037663c7
pawelztef <pawelztef@gmial.com>
2016-11-28 18:29:22 +0000

    gem gritter - nicer flash messages

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
+//= require gritter
 //= require_tree .
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,12 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to users_url, notice: 'User was succesfully created.'
+      # redirect_to users_url, notice: 'User was succesfully created.'
+      gflash :success => 'User was succesfully created.'
+      redirect_to users_url
     else
-      flash[:alert] = 'There was a problem while creating user.'
+      # flash[:alert] = 'There was a problem while creating user.'
+      gflash :now, :error =>'There was a problem while creating user.'
       render :new
     end
   end
@@ -28,19 +31,24 @@ class UsersController < ApplicationController
 
   def update
     if @user.update_attributes user_params
-      redirect_to users_url, notice: 'User was succesfully updated.'
+      # redirect_to users_url, notice: 'User was succesfully updated.'
+      gflash :success => 'User was succesfully updated.'
+      redirect_to users_url
     else
-      flash[:alert] = 'There was a problem while updating user.'
+      # flash[:alert] = 'There was a problem while updating user.'
+      gflash :now, :error =>'There was a problem while updating user.'
       render :new
     end
   end
 
   def destroy
     @user.destroy
-    redirect_to users_url, notice: 'User was succesfully deleted'
+    # redirect_to users_url, notice: 'User was succesfully deleted'
+    gflash :success => 'User was succesfully updated.'
+    redirect_to users_url
   end
 
-private
+  private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
