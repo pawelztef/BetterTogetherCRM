b698aab8ea0aabe945d5f23ae71d9bd31386137d
pawelztef <pawelztef@gmial.com>
2016-12-02 13:07:28 +0000

    simple form validation errors and from checked and some   links improved

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ActiveRecord::Base
   EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
 
   validates :first_name, presence: true
-  validates :last_name, presence: true,
+  validates :last_name, presence: true
   validates :email, presence: true,
-    uniqueness: true,
-    # format: {with: EMAIL_REGEX},
-    confirmation: true
+                    uniqueness: true,
+                    # format: {with: EMAIL_REGEX},
+                    confirmation: true
 
   def fullname
     "#{self.first_name} #{self.last_name}"
