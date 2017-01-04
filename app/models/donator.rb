5fb82e8b3769dcef7a8779a71e7b08ab383d8689
pawelztef <pawelztef@gmail.com>
2017-01-04 10:06:17 +0000

    Method create_or_update in Donator model finshed

--- app/models/donator.rb
+++ app/models/donator.rb
@@ -13,7 +13,12 @@ class Donator < ActiveRecord::Base
                     confirmation: true
   validates :phone1, presence: true
   
-  def self.create_or_update first_name, last_name 
-    Donator.find_or_create_by(first_name: first_name, last_name: last_name) 
+  def self.create_or_update user
+    donator = Donator.where(first_name: user.first_name, last_name: user.last_name).first_or_create 
+    donator.email = user.email
+    donator.phone1 = user.phone1 
+    donator.phone2 = user.phone2 
+    donator.institution = user.institution
+    donator.save
   end
 end
