9a76088939226621638fc48a7b145b9240bd5361
pawelztef <pawelztef@gmail.com>
2017-01-04 00:15:05 +0000

    Method create_or_update created in Donator model for finding or creating new model instance

--- app/models/donator.rb
+++ app/models/donator.rb
@@ -1,4 +1,8 @@
 class Donator < ActiveRecord::Base
   include Reusable
   has_many :donations
+
+  def self.create_or_update first_name, last_name 
+    Donator.find_or_create_by(first_name: first_name, last_name: last_name) 
+  end
 end
