d36e83265c3e545638c19cb0ad59b97325d4d483
pawelztef <pawelztef@gmail.com>
2017-01-04 00:21:11 +0000

    Validation to Donator added

--- app/models/donator.rb
+++ app/models/donator.rb
@@ -1,7 +1,18 @@
 class Donator < ActiveRecord::Base
   include Reusable
   has_many :donations
+  has_one :location, as: :localizable, dependent: :destroy
+  before_validation :remove_white_spaces, only: [:first_name, :last_name, :email]
 
+  # EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+  validates :first_name, presence: true, uniqueness: {scope: :last_name}
+  validates :last_name, presence: true, uniqueness: {scope: :first_name}
+  validates :email, presence: true,
+                    uniqueness: true,
+                    # format: {with: EMAIL_REGEX},
+                    confirmation: true
+  validates :phone1, presence: true
+  
   def self.create_or_update first_name, last_name 
     Donator.find_or_create_by(first_name: first_name, last_name: last_name) 
   end
