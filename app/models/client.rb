946bd9f2d3c748bfaec863732e40ef521e43ce34
pawelztef <pawelztef@gmail.com>
2017-01-06 10:45:00 +0000

    Method initialize_or_update copied from Donator into Client as a part of implementing adding dog from Client#new action.

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,7 +2,6 @@ class Client < ActiveRecord::Base
   include Reusable
   before_validation :remove_white_spaces, only: [:first_name, :last_name, :email]
   has_many :dogs, dependent: :destroy
-  has_many :donations
   has_one :location, as: :localizable, dependent: :destroy
 
   # EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
@@ -13,13 +12,22 @@ class Client < ActiveRecord::Base
                     # format: {with: EMAIL_REGEX},
                     confirmation: true
   validates :phone1, presence: true
-  # validates_associated :dogs
-  
 
   accepts_nested_attributes_for :dogs
-  accepts_nested_attributes_for :donations
   accepts_nested_attributes_for :location
 
+  def self.initialize_or_update attributes 
+    client = Client.where(first_name: attributes[:donator_attributes][:first_name],
+                            last_name: attributes[:donator_attributes][:last_name])
+                     .first_or_initialize
+
+    client.assign_attributes({ email: attributes[:donator_attributes][:email],
+                                phone1: attributes[:donator_attributes][:phone1],
+                                phone2: attributes[:donator_attributes][:phone2],
+                                institution: attributes[:donator_attributes][:institution],
+                                location: Location.new(attributes[:donator_attributes][:location_attributes])})
+    return client
+  end
 
 
 end
