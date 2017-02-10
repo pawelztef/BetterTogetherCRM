e993ae4ba2ef083401a20d8aee49a9307cafff11
pawelztef <pawelztef@gmail.com>
2017-02-10 13:45:53 +0000

    Event#create custom_event form part done

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,6 @@
 class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
+  before_action :set_tables, only: [:new, :show, :edit, :update, :destroy]
 
   # GET /events
   # GET /events.json
@@ -18,17 +19,11 @@ class EventsController < ApplicationController
 
   # GET /events/new
   def new
-    @clients = Client.all
-    @dogs = Dog.all
-    @volunteers = Volunteer.all
-
     @event = Event.new
     @note = Note.new
     @custom_event = CustomEvent.new
-
     @custom_event.notes << @note
     @event.custom_events << @custom_event
-
     respond_to do |format|
       format.html
       format.js
@@ -39,9 +34,9 @@ class EventsController < ApplicationController
   end
 
   def create
-    @dogs = Dog.find(params[:dogs_ids])
-    byebug
     @event = Event.new(event_params)
+    build_relevant
+    byebug
     respond_to do |format|
       if @event.save
         format.html
@@ -78,8 +73,46 @@ class EventsController < ApplicationController
   end
 
   private
+  def build_relevant
+    case params[:options]
+    when "1" #custom
+      build_custom_event
+    when "2" #training
+      build_training
+    when "3" #transfer
+      build_transfer
+    when "4" #visit
+      build_visit
+    else
+      # need to implement
+    end
+  end
+
+  def build_custom_event
+    @custom_event = @event.custom_events.first
+    @custom_event.clients << Client.find(params[:clients_ids]) if params[:clients_ids]
+    @custom_event.volunteers << Volunteer.find(params[:volunteers_ids]) if params[:volunteers_ids]
+    @custom_event.dogs << Dog.find(params[:dogs_ids]) if params[:dogs_ids] 
+    # @event.custom_events << @custom_event 
+  end
+
+  def build_training
+  end
+
+  def build_transfer
+  end
+
+  def build_visit
+  end
+
   def set_event
     @event = Event.find(params[:id])
+  end
+
+  def set_tables
+    @clients = Client.all
+    @dogs = Dog.all
+    @volunteers = Volunteer.all
   end
 
   def event_params
@@ -88,24 +121,6 @@ class EventsController < ApplicationController
                                   custom_events_attributes: [:description, 
                                                              dogs_ids: [],
                                                              clients_ids: [],
-                                                             volunteers_ids: []] )
+                                                             volunteers_ids: []])
   end
-
-
-  def build_relevant event
-    case params[:options]
-      when "0"
-        return event
-      when "1"
-        return event
-      when "2"
-        return event
-      when "3"
-        return event
-      when "4"
-        return event
-    end
-  end
-
-
 end
