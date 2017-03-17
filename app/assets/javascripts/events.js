2b2bec05e9709442f2d115e6aa7245682e3a5a9f
pawelztef <pawelztef@gmail.com>
2017-03-17 11:21:23 +0000

    Select event type menu fixed

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,33 +1,33 @@
-  function hide(selectors) {
-    return selectors.map(function(selector) {
-      selector.slideUp();
-    });
-  }
+// var hide = function( ...selectors) {
+//     return selectors.map(function(selector) {
+//       selector.slideUp();
+//     });
+//   }
 
-$(document).on('change load', '#options', function() {
-  var customEvent = $('#custom-event');
-  var training = $('#training');
-  var transfer = $('#transfer');
-  var visit = $('#visit');
-  var x = parseInt($('#options').val());
-  console.log(x);
-  switch(x) {
-    case 1:
-      $.when(hide(training, transfer, visit)).done(customEvent.slideDown());
-      break;
-    case 2:
-      $.when(hide(customEvent, transfer, visit)).done(training.slideDown());
-      break;
-    case 3:
-      $.when(hide(customEvent, training, visit)).done(transfer.slideDown());
-      break;
-    case 4:
-      $.when(hide(customEvent, training, transfer)).done(visit.slideDown());
-      break;
-    default:
-      hide(customEvent, training, transfer, visit);
-  }
-});
+// $(document).on('change load', '#options', function() {
+//   var customEvent = $('#custom-event');
+//   var training = $('#training');
+//   var transfer = $('#transfer');
+//   var visit = $('#visit');
+//   var x = parseInt($('#options').val());
+//   console.log(x);
+//   switch(x) {
+//     case 1:
+//       $.when(hide(training, transfer, visit)).done(customEvent.slideDown());
+//       break;
+//     case 2:
+//       $.when(hide(customEvent, transfer, visit)).done(training.slideDown());
+//       break;
+//     case 3:
+//       $.when(hide(customEvent, training, visit)).done(transfer.slideDown());
+//       break;
+//     case 4:
+//       $.when(hide(customEvent, training, transfer)).done(visit.slideDown());
+//       break;
+//     default:
+//       hide(customEvent, training, transfer, visit);
+//   }
+// });
 
 $(document).on('change load', '#training-other-checkbox', function() {
   if ( $("#training-other-checkbox").is(':checked')) {
