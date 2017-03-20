eb7507e1f42787de376cd76dc0a49ad3b6d3db8b
pawelztef <pawelztef@gmail.com>
2017-03-20 14:10:33 +0000

    Dashboard first step

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class DashboardController < ApplicationController
   def index
+    @events = Event.all.order("id desc").limit(5)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,28 @@ module ApplicationHelper
       "Welcome #{current_moderator.fullname}"
     end
   end
+
+  def event_type event
+    if event.custom_event.present?
+      return "custom event"
+    elsif event.training.present?
+      return "training"
+    elsif event.transfer.present?
+      return "transfer"
+    elsif event.visit.present?
+      return "visit"
+    else
+      return "undefined"
+    end
+  end
+
+  def stringify_event_dates event
+    date = event.start.to_date 
+    if event.start.day == event.end.day
+      "<span> One day event</span><br> <span>starts at: #{event.start.to_formatted_s(:short)}</span>".html_safe
+    else
+      "<span>Starts at: #{event.start.to_formatted_s(:short)}</span><br>Finishes on: #{event.end.to_formatted_s(:short)}".html_safe
+    end
+  end
+
 end
