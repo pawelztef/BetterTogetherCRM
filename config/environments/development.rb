3accd4978f24848a2ad273fc78548668792336fd
pawelztef <pawelztef@gmail.com>
2017-04-26 09:45:35 +0100

    deleting all precompiled stuff

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,4 +41,9 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  #
+  #
+  # config.assets.serve_static_files = true 
+  # config.serve_static_files = true
+  # config.assets.compile = true 
 end
