d48ad61e6150067081616d0ade7a1ce3d7d18909
pawelztef <pawelztef@gmail.com>
2017-06-07 13:00:18 +0100

    updating mailer

--- app/mailers/volunteer_mailer.rb
+++ app/mailers/volunteer_mailer.rb
@@ -1,10 +1,6 @@
 class VolunteerMailer < ApplicationMailer
+  default from: "BetterToggether"
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.volunteer_mailer.welcome_mail.subject
-  #
   def welcome_mail(volunteer)
     @volunteer = volunteer
     @email_template = WelcomeMail.pull 
